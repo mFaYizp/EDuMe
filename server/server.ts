4a771d7d49254d0605f1c1551a97c90414ae7211
mFaYizp <fayizfaz78@gmail.com>
2023-09-30 21:32:38 +0530

    db connected

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import { app } from "./app";
+import connectDB from "./utils/db";
 require("dotenv").config();
 
 //CREATE SERVER
 
-app.listen(process.env.PORT, () =>
-  console.log(`server is connected on Port ${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => {
+  console.log(`server is connected on Port ${process.env.PORT}`);
+  connectDB();
+});
