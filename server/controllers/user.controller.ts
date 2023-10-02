8ca0291519e8b147d2732683352143054d8414ba
mFaYizp <fayizfaz78@gmail.com>
2023-10-02 21:53:57 +0530

    authorized user middleware

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,7 @@ import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 require("dotenv").config();
 
 // register user
@@ -159,6 +160,10 @@ export const logoutUser = catchAsyncError(
     try {
       res.cookie("access_token", "", { maxAge: 1 });
       res.cookie("refresh_token", "", { maxAge: 1 });
+
+      const userId = req.user?._id || "";
+      redis.del(userId);
+
       res
         .status(200)
         .json({ success: true, message: "Logged out successfully" });
