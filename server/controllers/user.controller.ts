954ce9a995311b72577ecb7300198ed424c7ecc0
mFaYizp <fayizfaz78@gmail.com>
2023-10-02 11:14:42 +0530

    user activation

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsyncError } from "../middleware/catchAsyncErrors";
-import userModel from "../models/user.model";
+import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
@@ -80,3 +80,41 @@ export const createActivationToken = (user: any): IActivationToken => {
 
   return { token, activationCode };
 };
+
+// Activation user
+
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+
+export const activateUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalid activation code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+
+      const existUser = await userModel.findOne({email})
+      if (existUser) {
+        return next(new ErrorHandler("User already exists",400))
+      }
+
+      const user = await userModel.create({name,email,password})
+
+      res.status(200).json( {success: true,message:"user activated"})
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message,400))
+    }
+  }
+);
