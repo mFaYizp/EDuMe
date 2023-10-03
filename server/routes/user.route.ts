b3f85c2fa14c363f582554102793f33621428a1c
mFaYizp <fayizfaz78@gmail.com>
2023-10-03 16:29:07 +0530

    update password

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,6 +7,7 @@ import {
   registrationUser,
   socialAuth,
   updateAccessToken,
+  updatePassword,
   updateUserInfo,
 } from "../controllers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
@@ -27,5 +28,7 @@ userRouter.get("/me", isAuthenticated, getUserInfo);
 userRouter.post("/social-auth", socialAuth);
 
 userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
 
 export default userRouter;
