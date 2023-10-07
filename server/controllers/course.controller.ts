a06fbb269e00555335395a3e6f4e9224f2681adb
mFaYizp <fayizfaz78@gmail.com>
2023-10-07 09:38:23 +0530

    review feature added

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -9,7 +9,6 @@ import ejs from "ejs";
 import mongoose from "mongoose";
 import path from "path";
 import sendMail from "../utils/sendMail";
-import { IUser } from "../models/user.model";
 
 //  upload Course
 
@@ -264,6 +263,65 @@ export const addAnswer = catchAsyncError(
       res.status(200).json({ success: true, course });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+//  add review in course
+interface IAddReviewData {
+  review: string;
+  rating: number;
+  userId: string;
+}
+
+export const addReview = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req.user?.courses;
+
+      const courseId = req.params.id;
+
+      const courseExists = userCourseList?.some(
+        (course: any) => course._id.toString() === courseId.toString()
+      );
+      if (!courseExists) {
+        return next(
+          new ErrorHandler("Your not eligible to access this course", 404)
+        );
+      }
+
+      const course = await courseModel.findById(courseId);
+      const { review, rating } = req.body as IAddReviewData;
+
+      const reviewData: any = {
+        user: req.user,
+        comment: review,
+        rating,
+      };
+      course?.reviews.push(reviewData);
+
+      let avg = 0;
+
+      course?.reviews.forEach((rev: any) => {
+        avg += rev.rating;
+      });
+
+      if (course) {
+        course.ratings = avg / course.reviews.length;
+      }
+
+      await course?.save();
+
+      const notification = {
+        title: "New Review Received",
+        message: `${req.user?.name} has given a review in ${course?.name}`,
+      };
+
+      // create notification
+
+      res.status(200).json({ success: true, course });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.massage, 500));
     }
   }
 );
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   addAnswer,
   addQuestion,
+  addReview,
   editCourse,
   getAllCourses,
   getCourseByUser,
@@ -35,5 +36,7 @@ courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
 courseRouter.put("/add-question", isAuthenticated, addQuestion);
 
 courseRouter.put("/add-answer", isAuthenticated, addAnswer);
+
+courseRouter.put("/add-review/:id", isAuthenticated, addReview);
 
 export default courseRouter;
