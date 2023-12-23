3c78f2bcdbce0a343e6e3cfd370b0067401aff35
mFaYizp <fayizfaz78@gmail.com>
2023-12-23 23:22:46 +0530

    working on profile page

---
+++ client/app/hooks/useProtected.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import React from "react";
+import useUserAuth from "./userAuth";
+
+interface ProtectedProps {
+  children: React.ReactNode;
+}
+
+export default function Protected({ children }: ProtectedProps) {
+  const isAuthenticated = useUserAuth();
+
+  return isAuthenticated ? children : redirect("/");
+}
