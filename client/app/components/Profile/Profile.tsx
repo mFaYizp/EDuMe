2783e1c659ff17ffdf922e321c83077c0ca46a7d
mFaYizp <fayizfaz78@gmail.com>
2023-12-26 02:20:28 +0530

    profikr-password updated

--- client/app/components/Profile/Profile.tsx
+++ client/app/components/Profile/Profile.tsx
@@ -5,6 +5,7 @@ import { useLogoutQuery } from "../../../redux/features/auth/authApi";
 import { signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 import ProfileInfo from "./ProfileInfo";
+import ChangePassword from "./ChangePassword";
 
 type Props = {
   user: any;
@@ -51,6 +52,11 @@ const Profile: FC<Props> = ({ user }) => {
       {active === 1 && (
         <div className="w-full h-full bg-transparent mt-[80px]">
           <ProfileInfo user={user} avatar={avatar} />
+        </div>
+      )}
+      {active === 2 && (
+        <div className="w-full h-full bg-transparent mt-[80px]">
+          <ChangePassword />
         </div>
       )}
     </div>
