1be2028f3143962a322449cbc9dbce564cd36d7e
mFaYizp <fayizfaz78@gmail.com>
2024-01-10 22:56:12 +0530

    analytics success

---
+++ client/app/admin/orders-analytics/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Heading from "@/app/utils/Heading";
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import React from "react";
+import OrdersAnalytics from "../../components/Admin/Analytics/OrdersAnalytics";
+import DashboardHeader from "@/app/components/Admin/DashboardHeader";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Heading
+        title={`EDuMe - Admin`}
+        description="EDuMe is a platform for students to learn and get help from teachers"
+        keywords="Programming,MERN,Redux"
+      />
+      <div className="flex ">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSidebar />
+        </div>
+        <div className="w-[85%] ">
+          <DashboardHeader />
+          <OrdersAnalytics />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ client/app/admin/users-analytics/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Heading from "@/app/utils/Heading";
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import React from "react";
+import UsersAnalytics from "../../components/Admin/Analytics/UsersAnalytics";
+import DashboardHeader from "@/app/components/Admin/DashboardHeader";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Heading
+        title={`EDuMe - Admin`}
+        description="EDuMe is a platform for students to learn and get help from teachers"
+        keywords="Programming,MERN,Redux"
+      />
+      <div className="flex ">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSidebar />
+        </div>
+        <div className="w-[85%] ">
+          <DashboardHeader />
+          <UsersAnalytics />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
